6bbd2048c1a1a5635da92f9ba7187f73bd3c122c
Thomas Johander <lopan@Thomas-Johanders-MacBook-Pro.local>
2017-02-13 10:54:29 -0600

    Base Mode Working

---
+++ client.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  $('#submitNewEmployee').on('click', function(){
+    console.log('submit button clicked')
+    var firstName = $('#firstName').val()
+    var lastName = $('#lastName').val()
+    var idNumber = $('#idNumber').val()
+    var jobTitle = $('#jobTitle').val()
+    var annualSalary = $('#annualSalary').val()
+
+    //adds new employee row to Dom
+    $('#employeeTableBody').append(
+      '<tr>' +
+      '<td>' + firstName + '</td>' +
+      '<td>' + lastName + '</td>' +
+      '<td>' + idNumber + '</td>' +
+      '<td>' + jobTitle + '</td>' +
+      '<td>' + annualSalary + '</td>' +
+      '<td><button class="deleteEmployeeButton">Delete ' + firstName + '</button></td>' +
+      '</tr>'
+    );
+    // Add Monthly salary expenses to the Dom
+    var newEmployeeMonthlyExpenses = annualSalary / 12;
+    var previousSalaryTotal = $('#monthlyExpenses').text();
+    var totalMonthlyExpenses = parseInt(previousSalaryTotal) + newEmployeeMonthlyExpenses;
+    $('#monthlyExpenses').text(totalMonthlyExpenses);
+
+
+
+  });
+
+
+});
